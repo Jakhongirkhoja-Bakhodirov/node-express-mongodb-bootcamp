6c172a2678766225a5b60d21d447d5a775d9acce
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-01 04:48:58 +0500

    starting implement our api:handling get request

--- app.js
+++ app.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.get('/' , (req,res) => {
     res.status(200).json({
         'message':'Something new'
+    });
+});
+
+const tours = JSON.parse(
+    fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours' , (req,res) => {
+    res.statusCode = 200;
+    res.json({
+        'status':true,
+        'length':tours.length,
+        'data':{
+            'tours':tours
+        }
     });
 });
 
