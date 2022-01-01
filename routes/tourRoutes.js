8f5d030b91f7ef9f504cb44c3e04c91b2f9bcbdc
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-01 16:55:30 +0500

    created controllers

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,107 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-
-
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
-
-const getAllTours = (req,res) => {
-    
-    res.status(200).json({
-        status:true,
-        length:tours.length,
-        request_time:req.requestTime,
-        data:{
-            tours:tours
-        }
-    });
-}
-
-const addNewTour = (req,res) => {
-    const data = req.body
-
-    const id = tours[tours.length-1].id+1;
-    const newTour = Object.assign({
-        id:id
-    },data);
-
-    tours.push(newTour);
-
-    fs.writeFile(`${__dirname}/../dev-data/data/tours-simple.json`,JSON.stringify(tours) , (err) => {
-        if(err) {
-            res.status(500).json({
-                status:false,
-                message:'something went wrong',
-                error_message:err.message
-            });
-        }   
-            
-        res.status(200).json({
-            status:true,
-            data : newTour
-        });
-    });
-}
-
-const updateTour = (req,res) => {
-    const id = req.params.id*1;
-    const tour = tours.find(el => el.id == id)
-
-    if(tour) {
-        res.status(200).json({
-            status:true,
-            data:{
-                tour:req.body
-            }
-        });
-    } else {
-        res.status(404).json({
-            status:false,
-            message:'Not found'
-        });
-    }
-}
-
-const deleteTour = (req,res) => {
-    res.status(204).json({
-        status:true,
-        data:null
-    });
-}
-
-const getTourById = (req,res) => {
-
-    const id = req.params.id*1;
-
-    const tour = tours.find(el => el.id == id)
-
-    if(tour) {
-        res.status(200).json({
-            status:true,
-            data:{
-                tour:tour
-            }
-        });
-    } else {
-        res.status(404).json({
-            status:false,
-            message:'Not found'
-        });
-    }
-}
-
-
+const tourController = require('../controllers/toursController');
 router
     .route('/')
-    .get(getAllTours)
-    .post(addNewTour);
+    .get(tourController.getAllTours)
+    .post(tourController.addNewTour);
 
 router
     .route('/:id')
-    .get(getTourById)
-    .patch(updateTour)
-    .delete(deleteTour);
+    .get(tourController.getAllTours)
+    .get(tourController.getTourById)
+    .patch(tourController.updateTour)
+    .delete(tourController.deleteTour);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,110 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
-
-
-const users = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/users.json`)
-);
-
-const getAllUsers = (req,res) => {
-    console.log(req.requestTime);
-    res.statusCode = 200;
-    res.json({
-        status:true,
-        length:users.length,
-        request_time:req.requestTime,
-        data:{
-            users:users
-        }
-    });
-}
-
-const addNewUser = (req,res) => {
-    const data = req.body
-
-    const id = users[users.length-1].id+1;
-    const newTour = Object.assign({
-        id:id
-    },data);
-
-    users.push(newTour);
-
-    fs.writeFile(`${__dirname}/dev-data/data/users-simple.json`,JSON.stringify(users) , (err) => {
-        if(err) {
-            res.status(500).json({
-                status:false,
-                message:'something went wrong',
-                error_message:err.message
-            });
-        }   
-            
-        res.status(200).json({
-            status:true,
-            data : newTour
-        });
-    });
-}
-
-const updateUser = (req,res) => {
-    const id = req.params.id*1;
-    const tour = users.find(el => el.id == id)
-
-    if(tour) {
-        res.status(200).json({
-            status:true,
-            data:{
-                tour:req.body
-            }
-        });
-    } else {
-        res.status(404).json({
-            status:false,
-            message:'Not found'
-        });
-    }
-}
-
-const deleteUser = (req,res) => {
-    res.status(204).json({
-        status:true,
-        data:null
-    });
-}
-
-const getUserById = (req,res) => {
-
-    const id = req.params.id*1;
-
-    const tour = users.find(el => el.id == id)
-
-    if(tour) {
-        res.status(200).json({
-            status:true,
-            data:{
-                tour:tour
-            }
-        });
-    } else {
-        res.status(404).json({
-            status:false,
-            message:'Not found'
-        });
-    }
-}
- 
-
-
+const userController = require('../controllers/usersController');
 router
     .route('/')
-    .get(getAllUsers)
-    .post(addNewUser);
+    .get(userController.getAllUsers)
+    .post(userController.addNewUser);
 
 router
     .route('/:id')
-    .get(getUserById)
-    .patch(updateUser)
-    .delete(deleteUser);
+    .get(userController.getUserById)
+    .patch(userController.updateUser)
+    .delete(userController.deleteUser);
 
 
 module.exports = router;
