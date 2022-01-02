2651e525d6ec6bcafe5ed12343b64af67671caa3
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-02 22:49:00 +0500

    created simple mongoose schema

--- app.js
+++ app.js
@@ -26,6 +26,24 @@ mongoose.connect(DB,{
     console.log(`Database connection error ${err}`);
 });
 
+const tourSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:[true , 'A tour name is required!'],
+        unique:true
+    },
+    rating:{
+        type:Number,
+        default:4.5
+    },
+    price:{
+        type:Number,
+        required:[true,'A tour price is required!']
+    }
+});
+
+const Tour = mongoose.Model('Tour' , tourSchema);
+
 //Declare Middlewares
 app.use(express.json());
 app.use(morgan('dev'));
