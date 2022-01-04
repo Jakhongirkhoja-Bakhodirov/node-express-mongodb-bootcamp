74b95b9284fd5789d149ebb3e2180280cc77f27c
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-04 07:39:22 +0500

    pagination mongoose

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -32,6 +32,19 @@ const getAllTours = async(req,res) => {
             query = query.select('-__v');
         }
 
+        //4)Pagination
+        const page = req.query.page*1 || 1;
+        const limit = req.query.limit*1 || 100;
+        const skip = (page - 1) * limit;
+
+        query = query.skip(skip).limit(limit);
+
+        if(req.query.page) {
+            const numTours = await Tour.countDocuments();
+            if(skip >= numTours) {
+                throw new Error('this is page does not exist!'); 
+            }
+        }
 
         //Execute query
         const tours = await query;
