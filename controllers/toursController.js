768fefa742e9ceb4fa42b80a474244e8776b01af
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-05 15:18:04 +0500

    aggregation pipeline get-montly-plan of tours

--- controllers/toursController.js
+++ controllers/toursController.js
@@ -165,6 +165,64 @@ const getTourStats = async(req,res) => {
     }
 }
 
+const getMonthlyPlan = async(req,res) => {
+    try{
+        const year = req.params.year * 1 || 1;
+        const plan = await Tour.aggregate([
+            {
+                $unwind:'$startDates'
+            },
+            {
+                $match:{
+                    startDates:{
+                        $gte:new Date(`${year}-01-01`),
+                        $lte:new Date(`${year}-12-31`)
+                    }
+                }
+            },
+            {
+                $group:{
+                    _id:{
+                        $month:'$startDates'
+                    },
+                    numOfTours:{
+                        $sum:1
+                    },
+                    tours:{
+                        $push:'$name'
+                    }
+                }
+            },
+            {
+                $addFields:{
+                    month:'$_id'
+                }
+            },
+            {
+                $project:{
+                    _id:0
+                }
+            },
+            {
+                $sort:{
+                    numOfTours:-1
+                }
+            }
+        ]);
+        res.status(200).json({
+            status:true,
+            result:plan.length,
+            data:{
+                plan
+            }
+        }); 
+    }catch(err){
+        res.status(401).json({
+            status:false,
+            message:err.message
+        }); 
+    }
+}
 
 module.exports = {
     getAllTours,
@@ -175,5 +233,6 @@ module.exports = {
     checkID,
     checkBody,
     aliasTopTours,
-    getTourStats
+    getTourStats,
+    getMonthlyPlan
 }
