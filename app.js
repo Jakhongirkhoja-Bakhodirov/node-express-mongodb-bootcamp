5714b3973fa655102baf92162c07e40a219ce8c4
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-11 13:44:56 +0500

    implement global error handling middleware

--- app.js
+++ app.js
@@ -21,11 +21,15 @@ app.use(express.static(`${__dirname}/public`))
 app.use('/api/v1/tours',tourRouter);
 app.use('/api/v1/users',userRouter);
 
-app.all('*' , (req,res) => {
-    res.status(404).json({
-        status:false,
-        message:`Can't find ${req.originalUrl} on this server`
-    });
+app.all('*' , (req,res,next) => {
+    // res.status(404).json({
+    //     status:false,
+    //     message:`Can't find ${req.originalUrl} on this server`
+    // });
+    const err = new Error(`Can't find ${req.originalUrl} on this server`);
+    err.status = 'fail';
+    err.statusCode = 404;
+    next(err);
 }); 
 
 app.use((req,res,next) => {
@@ -33,6 +37,18 @@ app.use((req,res,next) => {
     console.log('Hello from the test middleware')
     next();
 })
+
+app.use((err , req , res ,next) => {
+    
+    err.statusCode = err.statusCode || 500;
+
+    err.status = err.status || 'error';
+
+    res.status(err.statusCode).json({
+        status:err.status,
+        message:err.message
+    });
+});
 
 module.exports = app;
 
