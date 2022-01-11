909524af91d0b455faa1b8030269e94a5869423e
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-11 14:18:09 +0500

    better error handling and refactoring code

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+const AppError = require('./utils/appError');
+const globalErrorHandling = require('./controllers/errorController');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -22,33 +24,11 @@ app.use('/api/v1/tours',tourRouter);
 app.use('/api/v1/users',userRouter);
 
 app.all('*' , (req,res,next) => {
-    // res.status(404).json({
-    //     status:false,
-    //     message:`Can't find ${req.originalUrl} on this server`
-    // });
-    const err = new Error(`Can't find ${req.originalUrl} on this server`);
-    err.status = 'fail';
-    err.statusCode = 404;
-    next(err);
+    next(new AppError(`Can't find ${req.originalUrl} on this server` , 404));
 }); 
 
-app.use((req,res,next) => {
-    req.requestTime = new Date().toISOString();
-    console.log('Hello from the test middleware')
-    next();
-})
 
-app.use((err , req , res ,next) => {
-    
-    err.statusCode = err.statusCode || 500;
-
-    err.status = err.status || 'error';
-
-    res.status(err.statusCode).json({
-        status:err.status,
-        message:err.message
-    });
-});
+app.use(globalErrorHandling);
 
 module.exports = app;
 
