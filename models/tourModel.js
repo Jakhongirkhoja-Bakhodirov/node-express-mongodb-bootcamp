f0c80db0adde3070471c6dc06a39854f2df3e492
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-11 05:34:55 +0500

    data validators:built-in validators

--- models/tourModel.js
+++ models/tourModel.js
@@ -6,7 +6,9 @@ const tourSchema = new mongoose.Schema({
     name:{
         type:String,
         required:[true , 'A tour name is required!'],
-        unique:true
+        unique:true,
+        minlength:[10,'A tour must have more or equal than 10 characters'],
+        maxlength:[40,'A tour must have less or equal than 10 characters']
     },
     duration:{
         type:Number,
@@ -18,11 +20,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty:{
         type:String,
-        required:[true,'A tour must have a difficulty']
+        required:[true,'A tour must have a difficulty'],
+        enum:{
+            values:['easy' , 'medium' , 'difficult'],
+            message:'Difficulty is either:easy , medium , difficult'
+        }
     },
     rating:{
         type:Number,
-        default:4.5
+        default:4.5,
+        min:[1,'A rating must be above 1.0'],
+        max:[5,'A rating must be below 5.0']
     },
     price:{
         type:Number,
