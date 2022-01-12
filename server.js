2accd6381bbef9ed85e3caaec4e0ac3131b43422
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-13 01:57:55 +0500

    errors outside express:unhandled and uncaugth exceptions

--- server.js
+++ server.js
@@ -4,21 +4,34 @@ const mongoose = require('mongoose')
 //set-up app's running port
 const port = process.env.PORT || 3000;
 
+process.on('uncaughtException' , err => {
+    console.log('Uncaught Exception Shutting down ...')
+    console.log(err.name,err.message);
+    process.exit(1);
+});
+
 //connecting MongoDB database
 const DB = process.env.DATABASE.replace('<password>' , process.env.DATABASE_PASSWORD);
 
-mongoose.connect(process.env.DATABASE_LOCAL,{
+mongoose.connect(process.env.DATABASE,{
     useNewUrlParser:true,
     // useCreateIndex:true,
     // useFindAndModify:false
 }).then((con) => {
     console.log('Connected to database successfully!');
    // console.log(con.connections);
-}).catch((err) => {
-    console.log(`Database connection error ${err}`);
 });
 
-app.listen(port , () => {
+const server = app.listen(port , () => {
     console.log(`App is running on port ${port}`);
 });
+
+
+process.on('unhandledRejection' , (err) => {
+    console.log('Unhadled Rejection App shuting down ..');
+    console.log(err.name,err.message);
+    server.close(() => {
+        process.exit(1);
+    });
+})
 
