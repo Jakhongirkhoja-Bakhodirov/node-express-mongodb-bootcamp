100b93433f3a4405795ae91427436b2677a6eb1b
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-13 01:07:37 +0500

    hanle database validation error

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -10,6 +10,14 @@ const handleDuplicateFields = (err) => {
     return new AppError(message,400);
 }
 
+const handleValidationErrorDB = err => {
+    const errors = Object.values(err.errors).map(el => el.message);
+  
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+  };
+  
+
 const sendErrorDev = (err,res) => {
     res.status(err.statusCode).json({
         status:err.status,
@@ -53,6 +61,11 @@ module.exports = (err,req,res,next) => {
         if(err.code == '11000') {
             error = handleDuplicateFields(error);
         }
+
+        if(err.name == 'ValidationError') {
+            error = handleValidationErrorDB(error);
+        }
+
         sendErrorProd(error,res); 
     }
 }
