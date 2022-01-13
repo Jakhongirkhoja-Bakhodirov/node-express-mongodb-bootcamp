4fd3c1227783353486d93d3090c6a344cb4d1493
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-13 21:44:42 +0500

    json-web-token installed and created token for user log-in

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,24 @@
+const jwt = require('jsonwebtoken')
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
 
 const signUp = catchAsync(async(req,res,next) => {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        password_confirmation:req.body.password
+    });
+
+    const token = jwt.sign({id:newUser._id} , process.env.JWT_SECRET ,{ expiresIn:process.env.JWT_EXPIRES_IN});
+    
     res.status(201).json({
         status:true,
+        token,
         data:newUser
     });
-})
+});
+
 
 module.exports = {
     signUp
