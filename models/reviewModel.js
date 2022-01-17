7cbf4ed17289851dcbc0c99d03e437ca1d624a65
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-18 04:49:07 +0500

    Modelling:Review Model

---
+++ models/reviewModel.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const User = require('./userModel');
+const Tour = require('./tourModel');
+
+const reviewSchema = mongoose.Schema({
+   review:{
+       type:String,
+       required:[true,'A review can not be empty!'],
+       min:10
+   },
+   rating:{
+       type:Number,
+       min:1,
+       max:5
+   },
+   tour:{
+       type:mongoose.Schema.ObjectId,
+       required:[true,'A review must belongs to a tour!'],
+       ref:Tour
+   },
+   user:{
+       type:mongoose.Schema.ObjectId,
+       required:[true,'A review must belongs to a user!'],
+       ref:User
+   },
+   createdAt:{
+       type:Date,
+       default:Date.now()
+   }
+}, {  
+    toJSON:{virtuals:true},
+    toObject:{virtuals:true}
+});
+
+const Review = mongoose.model('Review',reviewSchema);
+
+module.exports = Review;
