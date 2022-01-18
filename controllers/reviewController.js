33fc237a5bf0d5c55fc647ebf902f9ad9f45b0ef
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-18 05:00:47 +0500

    created ReviewController with Logic CRUD

---
+++ controllers/reviewController.js
@@ -0,0 +1,65 @@
+const Review = require('../models/reviewModel');
+const AppError = require('../utils/appError');
+
+
+const getAllReviews = async(req,res) => {
+    const reviews = await Review.find();
+    res.status(200).json({
+        success:'success',
+        results:reviews.length,
+        data:reviews
+    });
+}
+
+const getReview = async(req,res,next) => {
+    const review = await Review.findById(req.params.id);
+    if(!review) {
+        return next(new AppError(`A review not found with following ${req.params.id} id` , 404));
+    }
+    res.status(200).json({
+        success:'success',
+        data:reviews
+    });
+}
+
+const createReiview = async(req,res) => {
+    const review = await Review.create(req.body);
+    res.status(201).json({
+        status:'success',
+        review
+    });
+}
+
+const updateReview = async(req,res,next) => {
+    const review = await Review.findById(req.params.id);
+    if(!review) {
+        return next(new AppError(`A review not found with following ${req.params.id} id` , 404));
+    }
+    review.update(req.body);
+
+    res.status(200).json({
+        status:'success',
+        review
+    });
+}
+
+const deleteReview = async(req,res,next) => {
+    const review = await Review.findByIdAndDelete(req.params.id);
+    if (!review) {
+        return next(new AppError(`No review found with that ${req.params.id} Id`, 404));
+      }
+    
+      res.status(204).json({
+        status: 'success',
+        data: null
+      });
+}
+
+
+module.exports = {
+    getAllReviews,
+    getReview,
+    createReiview,
+    updateReview,
+    deleteReview
+}
